10aaa709a8bc0819529fa0aac780ebf30a07e8e6
gulchinhuseynova <gulchinzh@gmail.com>
2025-12-06 11:30:13 -0500

    Hello Git! With Murodil Updated

--- src/Main_day01_CommitPushPull.java
+++ src/Main_day01_CommitPushPull.java
@@ -2,6 +2,7 @@ public class Main_day01_CommitPushPull {
 
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Updated code");
     }
 
 }
